e3c3c1e0bed6521429b8ae5f8bbfb13241e962e1
A.M <andares@outlook.com>
2020-04-17 17:36:48 +0800

    update

--- uninstall.fish
+++ uninstall.fish
@@ -1,8 +1,13 @@
 #!env fish
+
+function play-book
+  echo ashe is removed.
+end
+
 function ___uninstall-ashe
   rm -f $HOME/.config/fish/conf.d/play.fish
   rm -r $HOME/.local/share/ashe
-  echo ashe is removed.
+  play-book
 end
 
 ___uninstall-ashe
