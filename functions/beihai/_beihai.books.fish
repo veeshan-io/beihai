a722f92651deef10bf38bd9f0de2f668c36e5460
A.M <andares@outlook.com>
2020-06-30 18:28:24 +0800

    updat

--- functions/beihai/_beihai.books.fish
+++ functions/beihai/_beihai.books.fish
@@ -3,7 +3,7 @@ function _beihai.books -a name -d "Show Beihai books"
   for path in $bh_book_path
     for file in (string split ' ' (echo $path/*.yml))
       set -l item (string match -r '([^\/]+)\.yml$' $file)[2]
-      if test -z "$name"; and test "$item" = "$name"
+      if not test -z "$name"; and test "$item" = "$name"
         echo $file
         return 0
       end
--- functions/beihai/_beihai.invs.fish
+++ functions/beihai/_beihai.invs.fish
@@ -3,7 +3,7 @@ function _beihai.invs -a name -d "Show Beihai inventories"
   for path in $bh_inv_path
     for file in (string split ' ' (echo $path/*.inv))
       set -l item (string match -r '([^\/]+)\.inv$' $file)[2]
-      if test -z "$name"; and test "$item" = "$name"
+      if not test -z "$name"; and test "$item" = "$name"
         echo $file
         return 0
       end
