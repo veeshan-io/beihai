059b03d37c9a2a56c13d44f864771b6481269bb3
A.M <andares@outlook.com>
2020-06-30 19:23:20 +0800

    update

--- completions/beihai.fish
+++ completions/beihai.fish
@@ -7,7 +7,3 @@
 # feel free to remove completions directory from the project.
 
 complete -c beihai -x -d "Command" -a (_beihai.command) -n "__fish_use_subcommand"
-
-set -l invs (_beihai.invs)
-complete -c play -n "__koi_no_subcommand_from $invs" -x -d "Inventory" -a $invs
-complete -c play -n "__fish_seen_subcommand_from $invs" -x -d "Book" -a (_beihai.books)
---
+++ completions/play.fish
@@ -0,0 +1,3 @@
+set -l invs (_beihai.invs)
+complete -c play -n "__koi_no_subcommand_from $invs" -x -d "Inventory" -a $invs
+complete -c play -n "__fish_seen_subcommand_from $invs" -x -d "Book" -a (_beihai.books)
