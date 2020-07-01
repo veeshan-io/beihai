12642221cb69bb34c8fbeea35bbf3049c00c5b8d
A.M <andares@outlook.com>
2020-07-01 18:30:09 +0800

    update

--- completions/play.fish
+++ completions/play.fish
@@ -1,3 +1,4 @@
+complete -c play -n "__koi_seen_--" -e
 set -l invs (_beihai.invs)
 complete -c play -n "__koi_no_subcommand_from $invs" -x -d "Inventory" -a $invs
-complete -c play -n "__fish_seen_subcommand_from $invs" -x -d "Book" -a (_beihai.books)
+complete -c play -n "__koi_seen_subcommand_from $invs" -x -d "Book" -a (_beihai.books)
--- functions/play.fish
+++ functions/play.fish
@@ -1,16 +1,15 @@
 function play -d "Play Beihai book"
-  echo $argv
-  echo $argv[1]
+  set -l inventory $argv[1]
+  set -l books $argv[2..-1]
+  echo $inventory
+  echo $books
 
-  # set -l inventory $argv[1]
-  # set -l book $argv[2]
-
-  # if test -z "$inventory"; or test -z "$book"
-  #   echo Usage: ./play-book \<book\> \<inventory\>[ \<...extra\>]
-  #   echo - Books:
-  #   ___show-book $__DIR
-  #   return $OMF_UNKNOWN_OPT
-  # end
+  if test -z "$inventory"; or test -z "$books"
+    echo Usage: play \<inventory\>[ \<...extra\>]
+    echo - Books:
+    ___show-book $__DIR
+    return $OMF_UNKNOWN_OPT
+  end
 
   # set book $__DIR/$book.yml
   # if not test -f $book
