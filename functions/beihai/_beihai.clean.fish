1a09e3f129b5c524894afd10405a307b9f9c69a8
A.M <andares@outlook.com>
2020-07-03 19:15:23 +0800

    update

--- functions/beihai/_beihai.clean.fish
+++ functions/beihai/_beihai.clean.fish
@@ -1,3 +1,8 @@
 function _beihai.clean -d "Clean Beihai directory"
   koi renew-repos $bh_root
+  for path in $ops_repos_path
+    if test -z "$path"
+      koi renew-repos $path
+    end
+  end
 end
