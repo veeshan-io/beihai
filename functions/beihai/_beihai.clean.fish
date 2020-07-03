61febea57e105405b0088897fe8e4ccd9862388e
A.M <andares@outlook.com>
2020-07-03 19:16:59 +0800

    update

--- functions/beihai/_beihai.clean.fish
+++ functions/beihai/_beihai.clean.fish
@@ -1,5 +1,4 @@
 function _beihai.clean -d "Clean Beihai directory"
-  koi renew-repos $bh_root
   for path in $ops_repos_path
     if test -z "$path"
       koi renew-repos $path
--- init.fish
+++ init.fish
@@ -7,7 +7,7 @@
 
 # Set vars
 set -xg bh_root (dirname (status -f))
-set -xg ops_repos_path
+set -xg ops_repos_path $bh_root $ops_repos_path
 set -xg bh_book_path $bh_root/books
 set -xg bh_inv_path $bh_root/books/inventories
 
