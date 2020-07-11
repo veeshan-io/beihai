4791f649ad5c05cd4aff990125471928ea42c1e1
A.M <andares@outlook.com>
2020-07-11 15:20:56 +0800

    is not exists

--- functions/borne.fish
+++ functions/borne.fish
@@ -2,7 +2,7 @@ function borne -d "Init borne local"
   set -l name (hostname)
   set -l inv (beihai bornes $name)
   if test -z "$inv"
-    echo Inventory for borne $inv is not exists.
+    echo Inventory for borne $inv is not found.
     return $OMF_UNKNOWN_OPT
   end
 
