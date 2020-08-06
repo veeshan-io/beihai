77fc6e1a9a88b08b75e8f4bf900550e7b15ebbda
A.M <andares@outlook.com>
2020-08-06 18:14:15 +0800

    update

--- init.fish
+++ init.fish
@@ -10,12 +10,12 @@ set -xg bh_root (dirname (status -f))
 set -xg bh_book_path $bh_root/books
 set -xg bh_inv_path $bh_root/books/inventories
 
+# build-image 所用路径
+set -xg image_dockerfile_repos
+
 # beihai renew 不再更新自己的库
 # set -xg ops_repos_path $bh_root $ops_repos_path
 set -xg ops_repos_path
-
-# beihai build-image 所用路径
-set -xg image_dockerfile_repos
 
 # Autoload
 autoload $path/functions/beihai
