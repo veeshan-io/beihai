8ab048d7fa93ab24d5989d3eae9c403837745223
A.M <andares@outlook.com>
2020-08-06 17:43:23 +0800

    update

--- completions/beihai.fish
+++ completions/beihai.fish
@@ -7,27 +7,3 @@
 # feel free to remove completions directory from the project.
 
 complete -c beihai -x -d "Command" -a (_beihai.command) -n "__fish_use_subcommand"
-
-# build-image
-set -l majors
-set -l minors
-set -l major
-for path in $image_dockerfiles
-  if not test -d $path
-    continue
-  end
-
-  set major (string match -r '([^\/]+)$' $path)[2]
-  set majors $majors $major
-  set minors
-  for subpath in (echo $path/*)
-    if not test -d $path
-      continue
-    end
-    set minors $minors (string match -r '([^\/]+)$' $subpath)[2]
-  end
-  if test (count $minors) -gt 0
-    complete -c beihai -n "__koi_seen_subcommand_from $major" -x -d "Minor" -a $minors
-  end
-end
-complete -c beihai -n "__koi_seen_subcommand_from build-image" -x -d "Major" -a $majors
